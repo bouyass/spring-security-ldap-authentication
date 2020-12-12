2bba8bc0f6ded6f2923d47d3e4edfbf72f6c4f02
lyes makhloufi <lyes.makhloufi@outlook.fr>
2020-12-12 18:02:26 +0100

    Committing project

---
+++ src/test/java/com/lyes/springsecurityldapauthentication/SpringSecurityLdapAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyes.springsecurityldapauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
